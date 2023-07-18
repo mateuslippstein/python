4ddece6cc06d217144fd69269bd3a8f68e26ae5a
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-07-18 14:29:56 -0300

    beecrowd 1248

--- beecrowd/1248.py
+++ beecrowd/1248.py
@@ -2,19 +2,29 @@ def check_diet(diet, breakfast, lunch):
     if not diet and not breakfast and not lunch:
         return ""
 
-    remaining_chars = set(diet) - (set(breakfast) | set(lunch))
-    extra_chars = set(breakfast - diet) - set(lunch - diet)
+    remaining_food = []  # List to store characters remaining in the diet after meals
+    extra_food = []  # List to store characters eaten in excess or not present in the diet
+    diet = set(diet)  
 
-    if extra_chars or not remaining_chars:
-        return "CHEATER"
+    for char in diet:
+        if char in breakfast:
+            breakfast = breakfast.replace(char, '', 1)  # Remove character from breakfast once eaten
+        elif char in lunch:
+            lunch = lunch.replace(char, '', 1)  # Remove character from lunch once eaten
+        else:
+            remaining_food.append(char)  # Character not eaten
+
+    extra_food = list(breakfast + lunch)  # Combine remaining breakfast and lunch as extra_chars
+
+    if extra_food:
+        return "CHEATER"  # If there are extra_chars, it means the diet was violated
     else:
-        return ''.join(sorted(remaining_chars))
+        return ''.join(sorted(remaining_food))
 
-
-num_test_case = int(input())
+num_test_cases = int(input())
 results = []
 
-for _ in range(num_test_case):
+for _ in range(num_test_cases):
     diet = input()
     breakfast = input()
     lunch = input()
