e128f87fb1877e6c84de8ca7eae51521396d36c2
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-07-19 20:05:31 -0300

    exercise one

---
+++ machine_learning/part_one/data_preprocessing/exercises/exercise_one.py
@@ -0,0 +1,16 @@
+# Importing the necessary libraries
+import pandas as pd
+from sklearn.datasets import load_iris
+
+# Load iris dataset
+data = load_iris()
+
+# Identifying the features (independent variables) and the dependent variable
+print(data.data)
+print(data.target)
+
+# Create dataframe
+
+# Create matrix of features (X) and dependent variable vector (y)
+
+# Print your feature matrix (X) and dependent variable vector (y)
