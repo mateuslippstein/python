777bd7bce792d9a59c9c1dc6acfaf1cd063569a3
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-07-19 20:27:19 -0300

    preprocesing tools

--- machine_learning/part_one/data_preprocessing/data_preprocessing_tools.py
+++ machine_learning/part_one/data_preprocessing/data_preprocessing_tools.py
@@ -36,3 +36,11 @@ print(X_train)
 print(X_test)
 print(y_train)
 print(y_test)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train[:, 3:] = sc.fit_transform(X_train[:, 3:])
+X_test[:, 3:] = sc.transform(X_test[:, 3:])
+print(X_train)
+print(X_test)
