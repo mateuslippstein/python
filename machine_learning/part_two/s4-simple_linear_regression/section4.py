2886ce015f994f4ee751f1c4ba8d486f1e18f9de
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-07-26 21:01:16 -0300

    s4 training simple linear regression model

---
+++ machine_learning/part_two/s4-simple_linear_regression/section4.py
@@ -0,0 +1,20 @@
+# Simple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('machine_learning/part_two/s4-simple_linear_regression/Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Training the Simple Linear Regression model on the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
