d0af4a77033abfb7db9ac94e68b54a6ebbee19e5
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-08-02 20:18:20 -0300

    splitting and training

--- machine_learning/part_two/s5-multiple_linear_regression/section5.py
+++ machine_learning/part_two/s5-multiple_linear_regression/section5.py
@@ -10,3 +10,19 @@ dataset = pd.read_csv('machine_learning/part_two/s5-multiple_linear_regression/5
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
+
+# Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+print(X)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Training the Multiple Linear Regression model on the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
