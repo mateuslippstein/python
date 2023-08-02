9ec145367f0d8439ef9a04c8a27c3806ede5d257
Mateus Lippstein <mateus_lippstein@hotmail.com>
2023-08-02 19:54:10 -0300

    importing

--- machine_learning/part_two/s5-multiple_linear_regression/section5.py
+++ machine_learning/part_two/s5-multiple_linear_regression/section5.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
+dataset = pd.read_csv('machine_learning/part_two/s5-multiple_linear_regression/50_Startups.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
